baeb81ead310ac7630bb9b64766457f8bb33d70e
Jacob Malcom <jmalcom@defenders.org>
2016-10-12 11:31:34 -0400

    Initial commit; added function to make the <head>.

---
+++ R/head.R
@@ -0,0 +1,28 @@
+# BSD_2_clause
+
+#' Create the <head> for ESC dev index.html
+#'
+#' Edit this function if new styling components, names, etc. are needed
+#'
+#' @return An \code{htmltools tags$head} element
+#' @seealso \link{make_modals} \link{make_body}
+#' @export
+#' @examples
+#' \dontrun{
+#' make_head()
+#' }
+make_head <- function() {
+  head <- tags$head(
+    tags$meta(charset = "utf-8"),
+    tags$meta(`http-equiv`="X-UA-Compatible", content="IE=edge"),
+    tags$meta(name="viewport", content="width=device-width, initial-scale=1"),
+
+    tags$title("Defenders ESC Development Pages"),
+
+    tags$script(src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"),
+    tags$link(href="www/bootstrap/css/bootstrap.css", rel="stylesheet"),
+    tags$link(href="www/bootstrap/js/bootstrap.js", rel="stylesheet"),
+    tags$link(href="www/bootstrap/js/bootstrap.min.js", rel="stylesheet")
+  )
+  return(head)
+}
